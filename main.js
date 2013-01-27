ae299beb23aadf8bbaf5a23178e14169e5508246
Erwin <erwin.toni@gmail.com>
2013-01-27 15:18:53 -0800

    mongo connection, add MONGO_DB to .env

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var http                  = require('http');
 var router                = require('./router');
 var req_root              = require('./req_root');
 var req_file              = require('./req_file');
+var model                 = require('./model');
 
 // TODO: minify js and css as part of deployment process.
 // IDEA: minify at startup rather than as a build step.
@@ -13,6 +14,7 @@ var req_file              = require('./req_file');
 if (process.env.PORT       === undefined) throw new Error('PORT not defined');
 if (process.env.MONGO_PORT === undefined) throw new Error('MONGO_PORT not defined');
 if (process.env.MONGO_HOST === undefined) throw new Error('MONGO_HOST not defined');
+if (process.env.MONGO_DB   === undefined) throw new Error('MONGO_DB not defined');
 if (process.env.FB_APP_ID  === undefined) throw new Error('FB_APP_ID not defined');
 if (process.env.APP_VER    === undefined) throw new Error('APP_VER not defined');
 
@@ -21,15 +23,16 @@ if (process.env.APP_VER    === undefined) throw new Error('APP_VER not defined')
 process.env.PORT       = process.env.PORT       .replace(' ', '');
 process.env.MONGO_PORT = process.env.MONGO_PORT .replace(' ', '');
 process.env.MONGO_HOST = process.env.MONGO_HOST .replace(' ', '');
+process.env.MONGO_DB   = process.env.MONGO_DB   .replace(' ', '');
 process.env.FB_APP_ID  = process.env.FB_APP_ID  .replace(' ', '');
 process.env.APP_VER    = process.env.APP_VER    .replace(' ', '');
 
 // Run intializations before starting router.
-var n = 2;
+var n = 3;
 function onDone() {
   if (--n === 0) router.start();
 }
 req_root             .init(onDone);
 //req_screen_container .init(onDone);
 req_file             .init(onDone);
-//model                .init(onDone);
+model                .init(onDone);
